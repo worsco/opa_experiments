4e1cb0e40de96b6ef716e8c9bd1fb675f3b34596
Scott Worthington <scworthi@redhat.com>
2021-07-30 13:08:50 -0400

    only one echo needed

--- imagepolicy/install-just-gatekeeper.sh
+++ imagepolicy/install-just-gatekeeper.sh
@@ -6,7 +6,6 @@ oc apply -f 000-gatekeeper-cr-scw.yaml
 
 echo "sleep 15 seconds"
 sleep 15
-echo "sleep 15 seconds"
 oc apply -f 020-constrainttemplate-imagesinnamespace.yaml
 
 echo "sleep 15 seconds"
