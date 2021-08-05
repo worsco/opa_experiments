85c11d0c72618b1ddc0db76c5f219e2b27d9cc4b
Scott Worthington <scworthi@redhat.com>
2021-08-05 10:48:04 -0400

    fix removal order

--- imagepolicy/uninstall.sh
+++ imagepolicy/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-oc delete -f 020-constrainttemplate-imagesinnamespace.yaml
 oc delete -f 021-imagesinnamespace-busybox-in-ciao.yaml
+oc delete -f 020-constrainttemplate-imagesinnamespace.yaml
 
 oc delete -f 000-gatekeeper-cr-scw.yaml
 
